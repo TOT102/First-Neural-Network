71d2e37f948fdbaceeaea6759d6e8afec88f0c2d
TOT102 <73033834+TOT102@users.noreply.github.com>
2024-04-12 20:12:44 +0200

    Update main.py
    Add data split, plot function

--- main.py
+++ main.py
@@ -20,3 +20,31 @@ X = torch.arange(start, end, step).unsqueeze(dim=1)
 y = weight * X + bias
 
 # Split data into training and testing 80/20
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], X[:train_split]
+X_test, y_test = X[train_split:], X[train_split:]
+
+#len(y_train), len(y_test)
+# Function -> Plots training data, test data and compares predictions.
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+
+  plt.figure(figsize=(10, 7))
+
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+  
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+  if predictions is not None:
+    # Plot the predictions in red (predictions were made on the test data)
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+  # Show the legend
+  plt.legend(prop={"size": 14});
+
+plot_predictions();
